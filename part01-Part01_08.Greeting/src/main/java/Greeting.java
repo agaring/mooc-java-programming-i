74886f4a7d3ef18a8c10a61eeb5a548f4abcc31d
Arnaud GARING <arnaud.garing67@gmail.com>
2023-05-01 17:40:48 +0200

    Complete exercise 8

--- part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Greeting {
@@ -6,7 +5,11 @@ public class Greeting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("What's your name?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name);
 
     }
 }
