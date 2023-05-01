1f1c6fcc8ef72ac250c72c4d90aed31e8695eff7
Arnaud GARING <arnaud.garing67@gmail.com>
2023-05-01 16:40:29 +0200

    Complete exercise 2

--- part01-Part01_02.AdaLovelace/src/test/java/AdaLovelaceTest.java
+++ part01-Part01_02.AdaLovelace/src/test/java/AdaLovelaceTest.java
@@ -1,7 +1,7 @@
-
 import fi.helsinki.cs.tmc.edutestutils.MockStdio;
 import fi.helsinki.cs.tmc.edutestutils.Points;
 import org.junit.*;
+
 import static org.junit.Assert.assertEquals;
 
 @Points("01-02")
