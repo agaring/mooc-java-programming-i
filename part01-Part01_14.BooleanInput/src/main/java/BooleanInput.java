ac34c9563749d2cb1ef319d7c1111e5d89812de6
Arnaud GARING <arnaud.garing67@gmail.com>
2023-05-01 18:46:41 +0200

    Complete exercise 14

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class BooleanInput {
@@ -6,7 +5,8 @@ public class BooleanInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // write your program here
-
+        System.out.println("Write something:");
+        boolean t = Boolean.valueOf(scanner.nextLine());
+        System.out.println("True or false? " + t);
     }
 }
